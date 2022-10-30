8929f7847275f6cc80c75e12f2933ad8fe8e99be
Payal Varshney <payal.varshney2010@gmail.com>
2022-10-30 14:46:41 +0100

    Complete machine learning pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -19,18 +19,81 @@ from sklearn.model_selection import GridSearchCV
 
 
 def load_data(database_filepath):
-    pass
+    """
+    load data from database
+    """
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('messages', engine)  
+    X = df["message"]    
+    category_names=['related', 'request', 'offer','aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
+           'security', 'military', 'child_alone', 'water', 'food', 'shelter','clothing', 'money', 'missing_people',
+            'refugees', 'death', 'other_aid','infrastructure_related', 'transport', 'buildings', 'electricity',
+           'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure','weather_related', 'floods', 'storm', 
+            'fire', 'earthquake', 'cold','other_weather', 'direct_report']
+    Y = df[category_names]
+    X=tokenize(X)
+    return X,Y,category_names
+
+def clean_text(text):
+    '''
+    cleaning the text
+    '''
+    text = text.lower()
+    text = text.strip()    
+    text = re.sub(r"[-()\"#/$%&*<@>;:{}`+=~|.!?,'0-9]", "", text)
+    return text
 
 def tokenize(text):
-    pass
+    '''
+    Removing stopword from text and applying lemmatization
+    '''
+    text = text.apply(lambda x: clean_text(x))
+    stop = set(nltk.corpus.stopwords.words('english'))
+    # Create WordNetLemmatizer object
+    wnl = WordNetLemmatizer()
+    text = text.apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+    text = text.apply(lambda x: " ".join([wnl.lemmatize(word) for word in x.split()]))   #lemmatization
+    return text 
+
 
 def build_model():
-    pass
+    '''
+    build model pipeline and grid serach for optimal parameter
+    '''
+    pipeline = Pipeline([('tfidf',TfidfVectorizer()),
+                     ('clf',MultiOutputClassifier(RandomForestClassifier(random_state=1)))
+    
+                ])
+    parameters ={
+        
+        
+        'clf__estimator__min_samples_split': [2, 3, 4],
+        'clf__estimator__criterion' :['gini', 'entropy'],
+        
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1)
+
+    return cv
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    predicting on test data and adding f1 scroe, recall and precision for classes
+    '''
+    # predict on test data
+    Y_pred = model.predict(X_test)
+    for i in range(len(category_names)):
+        print(f'{category_names[i]} classification_report:')
+        print(classification_report(Y_test.iloc[:,i], Y_pred[:,i]))
+
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    Export model as a pickle file
+
+    '''
+   
+    pickle.dump(model,open(model_filepath,'wb'))
+
 
 def main():
     if len(sys.argv) == 3:
