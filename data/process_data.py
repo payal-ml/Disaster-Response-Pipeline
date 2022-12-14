8594dd10c613f6c411d043c2540cd8bc443afa86
Payal Varshney <payal.varshney2010@gmail.com>
2022-12-14 14:44:02 +0100

    Add database file

--- data/process_data.py
+++ data/process_data.py
@@ -36,7 +36,7 @@ def save_data(df, database_filename):
     '''
     
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('messages', engine, index=False)  
+    df.to_sql('messages', engine, index=False, if_exists='replace')  
 
 
 def main():
